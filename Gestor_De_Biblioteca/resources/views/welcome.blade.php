cf57fc322622fa9871bf936c36c1ded94becf536
GuadaFachinotti <guadafachinotti@gmail.com>
2024-11-17 21:50:54 -0300

    Fix centrado welcome

--- Gestor_De_Biblioteca/resources/views/welcome.blade.php
+++ Gestor_De_Biblioteca/resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
 
     <!-- Features Section -->
     <div class="row">
-        <div class="col-xl-3 col-md-6 mb-4">
+        <div class="col-xl-4 col-md-6 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
@@ -27,7 +27,7 @@
             </div>
         </div>
 
-        <div class="col-xl-3 col-md-6 mb-4">
+        <div class="col-xl-4 col-md-6 mb-4">
             <div class="card border-left-success shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
@@ -43,23 +43,7 @@
             </div>
         </div>
 
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-info shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Notificaciones</div>
-                            <div class="h5 mb-0 font-weight-normal text-gray-800">Recordatorios de devolución automáticos</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-bell fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-xl-3 col-md-6 mb-4">
+        <div class="col-xl-4 col-md-6 mb-4">
             <div class="card border-left-warning shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
@@ -78,7 +62,7 @@
 
     <!-- Call to Action -->
     <div class="row justify-content-center mt-4">
-        <div class="col-lg-8">
+        <div class="col-lg-12">
             <div class="card shadow mb-4">
                 <div class="card-body text-center">
                     <h4 class="mb-4">Comienza tu viaje literario hoy</h4>
@@ -121,7 +105,7 @@
 
     <!-- About Section -->
     <div class="row justify-content-center">
-        <div class="col-lg-8">
+        <div class="col-lg-12">
             <div class="card shadow mb-4">
                 <div class="card-header py-3">
                     <h6 class="m-0 font-weight-bold text-primary">Sobre ALBA Library</h6>
