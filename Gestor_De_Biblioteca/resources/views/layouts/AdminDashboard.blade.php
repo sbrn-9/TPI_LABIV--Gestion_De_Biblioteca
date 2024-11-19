85a622ffad5b6cc5dab2cde2031ec97a7eec9653
Juan M. Fernandez <juanma08072003@gmail.com>
2024-11-19 09:17:05 -0300

    Fix auth en Dropdown derecho

--- Gestor_De_Biblioteca/resources/views/layouts/AdminDashboard.blade.php
+++ Gestor_De_Biblioteca/resources/views/layouts/AdminDashboard.blade.php
@@ -177,32 +177,44 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                @auth
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">
-                                    {{ Auth::check() ? 'Holaa, ' . Auth::user()->name : 'Bienvenido' }}</span>
-
+                                    Holaa, {{Auth::user()->name}}
+                                </span>
                             </a>
-                            <!-- Dropdown - User Information -->
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Mi perfil
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Configuración
-                                </a>
+                                <!-- Dropdown - User Information -->
+                                <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                                    aria-labelledby="userDropdown">
+                                    <a class="dropdown-item" href="#">
+                                        <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                        Mi perfil
+                                    </a>
+                                    <a class="dropdown-item" href="#">
+                                        <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                                        Configuración
+                                    </a>
 
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="{{route('login')}}" data-toggle="modal" data-target="#logoutModal">
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Cerrar sesión
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="{{route('login')}}" data-toggle="modal" data-target="#logoutModal">
+                                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                        Cerrar sesión
+                                    </a>
+                                </div>
+                                @else
+                                    <span class="mr-2 d-none d-lg-inline text-gray-600 small">
+                                        Bienvenido
+                                    </span>
                                 </a>
-                            </div>
+                                    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                                    aria-labelledby="userDropdown">
+                                    <a class="dropdown-item" href="{{route('login')}}">
+                                        <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                        Iniciar Sesión
+                                    </a>
+                                @endauth
+
                         </li>
-
                     </ul>
-
                 </nav>
                 <!-- End of Topbar -->
 
